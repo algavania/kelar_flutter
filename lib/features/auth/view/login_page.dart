ba05d9bf295c6620e750f46eba6eeb2a3647f28d
Alga <vaniaalga@gmail.com>
2024-06-02 15:42:50 +0700

    style: login page

--- lib/features/auth/view/login_page.dart
+++ lib/features/auth/view/login_page.dart
@@ -1,5 +1,13 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:kelar_flutter/core/color_values.dart';
+import 'package:kelar_flutter/core/styles.dart';
+import 'package:kelar_flutter/l10n/l10n.dart';
+import 'package:kelar_flutter/widgets/custom_button.dart';
+import 'package:kelar_flutter/widgets/custom_gesture_unfocus.dart';
+import 'package:kelar_flutter/widgets/custom_text_field.dart';
+import 'package:sizer/sizer.dart';
+import 'package:unicons/unicons.dart';
 
 @RoutePage()
 class LoginPage extends StatefulWidget {
@@ -10,8 +18,77 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: CustomGestureUnfocus(
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Container(
+                  margin: const EdgeInsets.all(Styles.defaultPadding),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 5.h),
+                      SizedBox(height: 6.h),
+                      Text(
+                        context.l10n.loginText1,
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      SizedBox(height: 1.5.h),
+                      Text(
+                        context.l10n.loginText2,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodySmall
+                            ?.copyWith(color: ColorValues.grey50),
+                      ),
+                      SizedBox(height: 5.h),
+                      CustomTextField(
+                        textInputType: TextInputType.emailAddress,
+                        controller: _emailController,
+                        label: context.l10n.yourEmail,
+                        hint: context.l10n.enterEmail,
+                        icon: UniconsLine.envelope,
+                      ),
+                      const SizedBox(height: Styles.defaultSpacing),
+                      CustomTextField(
+                        controller: _passwordController,
+                        label: context.l10n.yourPassword,
+                        hint: context.l10n.enterPassword,
+                        icon: UniconsLine.key_skeleton,
+                        isPassword: true,
+                      ),
+                      const SizedBox(height: Styles.defaultSpacing),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(context.l10n.forgotPassword,
+                              style: Theme.of(context).textTheme.displaySmall,),
+                        ],
+                      ),
+                      const SizedBox(height: Styles.bigSpacing),
+                      CustomButton(
+                        text: context.l10n.login,
+                        width: double.infinity,
+                        onPressed: () {
+                        },
+                      ),
+                      Expanded(child: Container()),
+                      SizedBox(height: 3.h),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
